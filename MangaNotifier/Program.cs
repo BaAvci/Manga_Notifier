cbe9a25c53d64c194c9013c88bce5ddf8b7da7dc
Jisatzu <batuhan.avci@outlook.de>
2023-12-08 22:35:40 +0100

    Added discord bot integration
    Sends all existing comics to the discord bot

--- MangaNotifier/Program.cs
+++ MangaNotifier/Program.cs
@@ -7,10 +7,12 @@ namespace Manga_Notifier // Note: actual namespace depends on the project name.
 {
     internal class Program
     {
-        static void Main(string[] args)
+        private readonly ulong channelID = 1182731621737181225;
+        public static Task Main(string[] args) => new Program().StartMain();
+
+        public async Task StartMain()
         {
             Crawler crawler = new();
-
             List<IScanlators> scanlators = new()
             {
                 new Asurascans("https://asuratoon.com/manga/?order=update"),
@@ -18,18 +20,22 @@ namespace Manga_Notifier // Note: actual namespace depends on the project name.
                 //new Reaperscans("https://reaperscans.com/latest/comics")
             };
 
+            var bot = new Discord_Bot();
+            await bot.StartAsync(channelID);
             foreach (IScanlators scans in scanlators)
             {
                 crawler.Crawl(scans);
                 foreach (var series_Info in scans.SeriesInfo)
                 {
+                    Console.WriteLine(series_Info.Scanlator);
                     Console.WriteLine(series_Info.Name);
                     Console.WriteLine(series_Info.URL);
                     Console.WriteLine(series_Info.ChapterName);
-                    Console.WriteLine(series_Info.Id);
+                    await bot.Announce(series_Info.Scanlator, series_Info.Name, series_Info.ChapterName, series_Info.URL);
                 }
                 Thread.Sleep(5000);
             }
+            await bot.CloseAsync();
         }
     }
 }
