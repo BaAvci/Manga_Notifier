24e78c570b9deedc50cc47424da01767ad947d7a
Jisatzu <batuhan.avci@outlook.de>
2024-09-10 16:56:57 +0200

    Fixed Token Error

--- Unit_Tests/Crawling/DiscordBot_Tests.cs
+++ Unit_Tests/Crawling/DiscordBot_Tests.cs
@@ -1,26 +1,26 @@
-using Manga_Notifier;
 using Manga_Notifier.Scanlators;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-using Xunit.Sdk;
+using System.Collections;
+using Xunit.Abstractions;
 
 namespace Unit_Tests.Crawling
 {
-    public class DiscordBot_Tests
+    [Collection("LaunchSettingFixture")]
+    public class DiscordBot_Tests :IClassFixture<LaunchSettingFixture>
     {
-        private readonly Discord_Bot bot;
-        private SourceView_Path_finder path_Finder;
-        private Asurascans asurascans;
-        private ulong channelID = 1182733801194016818;
-        public DiscordBot_Tests() 
+        private Discord_Bot.Bot bot;
+        private readonly ITestOutputHelper _testOutputHelper;
+        private readonly SourceView_Path_finder path_Finder;
+        private readonly Asurascans asurascans;
+        private readonly ulong channelID = 1182733801194016818;
+        private readonly LaunchSettingFixture _fixture;
+
+        public DiscordBot_Tests(LaunchSettingFixture fixture, ITestOutputHelper testOutputHelper) 
         {
             path_Finder = new SourceView_Path_finder();
             asurascans = new Asurascans("https://asuratoon.com/manga/?order=update");
-
-            bot = new Discord_Bot();
+            _testOutputHelper = testOutputHelper;
+            _fixture = fixture;
+            bot = new Discord_Bot.Bot(channelID);
         }
 
         [Fact]
@@ -28,7 +28,7 @@ namespace Unit_Tests.Crawling
         {
             var webpage = File.ReadAllText(path_Finder.AsuraSingle1ComicPath);
             asurascans.ParseURLS(webpage);
-            await bot.StartAsync(channelID);
+            await bot.StartBot();
             await bot.Announce(scanlator: asurascans.SeriesInfo[0].Scanlator,
                                name: asurascans.SeriesInfo[0].Name,
                                chapter: asurascans.SeriesInfo[0].ChapterName,
@@ -38,11 +38,12 @@ namespace Unit_Tests.Crawling
             await bot.Delete();
 
             Assert.NotNull(result);
-            Assert.Equal(5, result.Count());
+            Assert.Equal(5, result.Length);
             Assert.Contains("Asuratoon",result[0]);
             Assert.Contains("SSS-Class Suicide Hunter", result[1]);
             Assert.Contains("Chapter 103", result[2]);
             Assert.Contains("https://asuratoon.com/7117659858-sss-class-suicide-hunter-chapter-103", result[4]);
+            await bot.StopBot();
         }
 
     }
