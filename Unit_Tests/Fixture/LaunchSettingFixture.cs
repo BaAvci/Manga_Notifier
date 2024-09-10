babf18a867d38f3232f5a6f8e173b56d3c9a1548
Jisatzu <batuhan.avci@outlook.de>
2024-09-10 17:17:00 +0200

    Test

--- Unit_Tests/Fixture/LaunchSettingFixture.cs
+++ Unit_Tests/Fixture/LaunchSettingFixture.cs
@@ -8,22 +8,25 @@ namespace Unit_Tests.Crawling
     {
         public LaunchSettingFixture()
         {
-            using(var file = File.OpenText("../../../Properties/launchSettings.json"))
+            if(System.IO.File.Exists("../../../Properties/launchSettings.json"))
             {
-                var reader = new JsonTextReader(file);
-                var jObject = JObject.Load(reader);
-
-                var variables = jObject.GetValue("profiles")
-                    //select a proper profile here
-                    .SelectMany(profiles => profiles.Children())
-                    .SelectMany(profiles => profiles.Children<JProperty>())
-                    .Where(prop => prop.Name == "environmentVariables")
-                    .SelectMany(prop => prop.Value.Children<JProperty>())
-                    .ToList();
-
-                foreach( var variable in variables)
+                using(var file = File.OpenText("../../../Properties/launchSettings.json"))
                 {
-                    Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+                    var reader = new JsonTextReader(file);
+                    var jObject = JObject.Load(reader);
+
+                    var variables = jObject.GetValue("profiles")
+                        //select a proper profile here
+                        .SelectMany(profiles => profiles.Children())
+                        .SelectMany(profiles => profiles.Children<JProperty>())
+                        .Where(prop => prop.Name == "environmentVariables")
+                        .SelectMany(prop => prop.Value.Children<JProperty>())
+                        .ToList();
+
+                    foreach(var variable in variables)
+                    {
+                        Environment.SetEnvironmentVariable(variable.Name, variable.Value.ToString());
+                    }
                 }
             }
         }
